8db13d80383852b9829b6847372198d547da46df
NickOsipov <nick.osipov.91@gmail.com>
2025-01-09 01:45:53 +0300

    fix: tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -6,6 +6,9 @@ from src.models import create_model
 
 
 def test_create_model():
+    """
+    Test create_model function.
+    """
     assert create_model() is not None
     assert hasattr(create_model(), "fit")
     assert hasattr(create_model(), "predict")
